6354000b7f3695f0e961357e0baac66760623db6
Renato Cantarino <renato.cantarino@gmail.com>
2019-06-25 14:03:20 -0300

    first commit

---
+++ RetryPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RetryPattern
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var tentativasMxima = 3;
+            var delayOperacao = TimeSpan.FromSeconds(2);
+            System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+            RetryHelper.RetryOnException(tentativasMxima, delayOperacao, () =>
+            {
+                var x = client.GetAsync("XXXXXXXXXXXX");
+            });
+        }
+    }
+}
---
+++ RetryPattern/RetryHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RetryPattern
+{
+    public static class RetryHelper
+    {
+        //private static ILog logger = LogManager.GetLogger();
+
+        public static void RetryOnException(int times, TimeSpan delay, Action operation)
+        {
+            var tentativa = 0;
+
+            do
+            {
+                try
+                {
+                    tentativa++;
+                    operation();
+                    break;
+                }
+                catch (Exception)
+                {
+                    if (tentativa == times)  // fim da execucao baseada na quantidade de tentativas
+                        throw;
+
+                    //logger.Error($"tentativa numero {attempts} - tentaremos novamente em {delay}", ex);
+                    Task.Delay(delay).Wait();
+                }
+            } while (true);
+        }
+    }
+}
